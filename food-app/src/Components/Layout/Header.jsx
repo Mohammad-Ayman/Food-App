c8d8a4cc6ea1ec59418cafc08c66e45ce6d220d4
Mohamed Ibrahim <mohdayman22rr@gmail.com>
2023-07-02 23:54:37 +0300

    first commit

---
+++ food-app/src/Components/Layout/Header.jsx
@@ -0,0 +1,21 @@
+import HeaderCartButton from "./HeaderCartButton";
+
+import styles from "./Header.module.css";
+
+import mealsImage from "../../assets/meals.jpg";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImage} alt="Table full of food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ food-app/src/Components/Meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,11 @@
+// import styles from "./styles/MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form>
+      <button></button>
+    </form>
+  );
+};
+
+export default MealItemForm;
